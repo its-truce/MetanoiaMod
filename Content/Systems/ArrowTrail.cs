d0728f9f66367bb4693b3ee6afde00e06fa28f3e
itstruce <107202816+its-truce@users.noreply.github.com>
2023-06-24 10:46:51 +0530

    Update ArrowTrail.cs

--- Content/Systems/ArrowTrail.cs
+++ Content/Systems/ArrowTrail.cs
@@ -1,4 +1,16 @@
-if (source is EntitySource_ItemUse_WithAmmo use && use.Item.type == ModContent.ItemType<ArtemisFowl>() || use.Item.type == ModContent.ItemType<ApolloSolstice>())
+// teria usings
+
+// class inherits from GlobalProjectile
+bool fromOlympusBows;
+
+// this goes under OnSpawn hook
+if (source is EntitySource_ItemUse_WithAmmo use && (use.Item.type == ModContent.ItemType<ArtemisFowl>() || use.Item.type == ModContent.ItemType<ApolloSolstice>()))
 {
   fromOlympusBows = true;
+}
+
+//override predraw here
+if (projectile.arrow && fromOlympusBows)
+{
+  // draw code
 }
