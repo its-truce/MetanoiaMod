07c8cd414b6e24944689760081a4b9a4fdd2cd64
itstruce <107202816+its-truce@users.noreply.github.com>
2023-06-24 10:58:32 +0530

    Update ArrowTrail.cs

--- Content/Systems/ArrowTrail.cs
+++ Content/Systems/ArrowTrail.cs
@@ -2,15 +2,26 @@
 
 // class inherits from GlobalProjectile
 bool fromOlympusBows;
+int itemType;
+Color drawColor;
 
 // this goes under OnSpawn hook
 if (source is EntitySource_ItemUse_WithAmmo use && (use.Item.type == ModContent.ItemType<ArtemisFowl>() || use.Item.type == ModContent.ItemType<ApolloSolstice>()))
 {
   fromOlympusBows = true;
+  itemType = use.Item.type;
 }
 
 //override predraw here
 if (projectile.arrow && fromOlympusBows)
 {
-  // draw code
+  if (itemType == ModContent.ItemType<ArtemisFowl>())
+  {
+    drawColor = Color.Green
+  }
+  if (itemType == ModContent.ItemType<ApolloSolstice>())
+  {
+    drawColor = Color.Gold
+  }
+  // draw code here, use drawColor, multiply by float, set .A to 0
 }
